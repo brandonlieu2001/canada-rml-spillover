ef1b2f375dc6d245974a9ea5443279827f322164
brandonlieu2001 <brandonlieu2001@gmail.com>
2025-11-10 23:16:11 -0800

    Add code for generating Idaho dataset

--- code/01_process_fars_data.R
+++ code/01_process_fars_data.R
@@ -19,4 +19,21 @@ for (i in 1:num_years) {
     left_join(person_data[[i]],  accident_data[[i]], by = "ST_CASE")
 }
 
-## Create individual state data
+## Create Idaho data across all years
+idaho = data.frame()
+
+for (year in year_directory) {
+  rows_to_add <- accident_person_data[[year]] %>% 
+    select(STATE.x, COUNTY.x, ST_CASE, YEAR, MONTH.x, PER_NO,
+           VEH_NO, PER_NO) %>% 
+    filter(STATE.x == 16)
+  idaho <- bind_rows(idaho, rows_to_add)
+}
+
+## Explore counts of border county in Idaho
+idaho %>% filter(COUNTY.x == 21) %>% 
+  group_by(YEAR, MONTH.x) %>% 
+  summarise(n_accidents = n_distinct(ST_CASE))
+
+## Write out Idaho data to `data_processed/`
+write_csv(idaho, "data_processed/idaho_accident_person_data.csv")
