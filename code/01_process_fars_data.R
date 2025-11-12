28ed984c5a712aff1711759fe26c735045b60c21
brandonlieu2001 <brandonlieu2001@gmail.com>
2025-11-11 22:43:22 -0800

    ID resolution exploration

--- code/01_process_fars_data.R
+++ code/01_process_fars_data.R
@@ -1,7 +1,8 @@
-## Import packages
+# Import packages
 library(tidyverse)
+library(lubridate) # to convert year-month to year-quarter
 
-## Read in `person.csv` & `accident.csv` from each year
+# Read in `person.csv` & `accident.csv` from each year
 year_directory <- list.files("data_raw")
 
 person_filepaths <- paste0("data_raw/", year_directory, "/person.csv")
@@ -10,7 +11,7 @@ accident_filepaths <- paste0("data_raw/", year_directory, "/accident.csv")
 person_data <- lapply(person_filepaths, read.csv)
 accident_data <- lapply(accident_filepaths, read.csv)
 
-## For each year, merge `person.csv` and `accident.csv`
+# For each year, merge `person.csv` and `accident.csv`
 accident_person_data <- list()
 num_years <- length(year_directory)
 
@@ -19,21 +20,59 @@ for (i in 1:num_years) {
     left_join(person_data[[i]],  accident_data[[i]], by = "ST_CASE")
 }
 
-## Create Idaho data across all years
+# Create Idaho data across all years
 idaho = data.frame()
-
 for (year in year_directory) {
   rows_to_add <- accident_person_data[[year]] %>% 
-    select(STATE.x, COUNTY.x, ST_CASE, YEAR, MONTH.x, PER_NO,
-           VEH_NO, PER_NO) %>% 
-    filter(STATE.x == 16)
+    select(STATE = STATE.x, COUNTY = COUNTY.x, ST_CASE, 
+           YEAR, MONTH = MONTH.x,
+           PER_NO, VEH_NO, ST_CASE, AGE) %>% 
+    filter(STATE == 16)
   idaho <- bind_rows(idaho, rows_to_add)
 }
 
-## Explore counts of border county in Idaho
-idaho %>% filter(COUNTY.x == 21) %>% 
-  group_by(YEAR, MONTH.x) %>% 
-  summarise(n_accidents = n_distinct(ST_CASE))
+# Calculate the number of accidents in all Idaho counties over time
+idaho_accidents <- idaho %>% 
+  group_by(YEAR, MONTH, COUNTY) %>% 
+  summarise(n_accidents = n_distinct(ST_CASE)) %>% 
+  arrange(COUNTY, YEAR, MONTH)
 
-## Write out Idaho data to `data_processed/`
-write_csv(idaho, "data_processed/idaho_accident_person_data.csv")
+# Create a full grid of all year-month-county combinations
+all_years_months <- expand_grid(
+  YEAR = unique(idaho$YEAR),
+  MONTH = unique(idaho$MONTH),
+  COUNTY = unique(idaho$COUNTY)
+)
+
+# Merge and fill missing counts with 0
+idaho_accidents_full_monthly <- all_years_months %>%
+  left_join(idaho_accidents, by = c("YEAR", "MONTH", "COUNTY")) %>%
+  mutate(n_accidents = replace_na(n_accidents, 0)) %>%
+  arrange(COUNTY, YEAR, MONTH)
+
+# Add date column
+idaho_accidents_full_monthly <- idaho_accidents_full_monthly %>% 
+  mutate(DATE = paste(YEAR, MONTH, sep = "-"))
+
+idaho_accidents_full_monthly$DATE <- 
+  as.Date(paste0(idaho_accidents_full_monthly$DATE, "-01"))
+
+# Plot using monthly data
+ggplot(data = idaho_accidents_full_monthly,
+       aes(x = DATE, 
+           y = n_accidents, color = as.factor(COUNTY))) +
+  geom_point() +
+  scale_y_continuous(breaks = seq(0, 100)) +
+  scale_x_date(date_labels = "%Y", 
+               date_breaks = "1 year") +
+  geom_vline(xintercept = as.Date("2018-10-15"), 
+             linetype = "dashed", color = "red", linewidth = 1) +
+  labs(x = "Time (year-month)", 
+       y = "Number of fatal accidents",
+       color = "County Code",
+       title = "Idaho: Fatal Accidents by County Per Month") +
+  theme_bw()
+
+# Write out Idaho data to `data_processed/`
+write_csv(idaho_accidents_full_monthly, 
+          "data_processed/idaho_accident_person_data.csv")
---
+++ code/02_resolution.R
@@ -0,0 +1,94 @@
+## Import packages
+library(tidyverse)
+library(lubridate) # to convert year-month to year-quarter
+
+## Explore resolution of data (aggregated by month, quarter, and year)
+
+# Import processed data
+idaho_accidents_full_monthly <- 
+  read.csv("data_processed/idaho_accident_person_data.csv")
+
+# Build quarterly dataset
+idaho_accidents_full_quarterly <- idaho_accidents_full_monthly %>%
+  mutate(QUARTER = quarter(DATE, with_year = TRUE)) %>%
+  group_by(QUARTER, COUNTY) %>%
+  summarise(n_accidents = sum(n_accidents)) %>%
+  arrange(COUNTY, QUARTER) %>% 
+  ungroup()
+
+# Build yearly dataset
+idaho_accidents_full_yearly <- idaho_accidents_full_monthly %>%
+  group_by(YEAR, COUNTY) %>%
+  summarise(n_accidents = sum(n_accidents)) %>%
+  arrange(COUNTY, YEAR) %>% 
+  ungroup()
+
+# Summary table of all three resolutions
+idaho_accidents_summary <- data.frame(
+  resolution = c("Monthly", "Quarterly", "Yearly"),
+  n_observations = c(
+    nrow(idaho_accidents_full_monthly),
+    nrow(idaho_accidents_full_quarterly),
+    nrow(idaho_accidents_full_yearly)
+  ),
+  max = c(
+    idaho_accidents_full_monthly %>% summarise(max_month = max(n_accidents)) %>% pull(),
+    idaho_accidents_full_quarterly %>% summarise(max_qtr = max(n_accidents)) %>% pull(),
+    idaho_accidents_full_yearly %>% summarise(max_year = max(n_accidents)) %>% pull()
+  ),
+  percent_zero = c(
+    idaho_accidents_full_monthly %>% summarise(pct_zero_month = mean(n_accidents == 0) * 100) %>% pull(),
+    idaho_accidents_full_quarterly %>% summarise(pct_zero_qtr = mean(n_accidents == 0) * 100) %>% pull(),
+    idaho_accidents_full_yearly %>% summarise(pct_zero_year = mean(n_accidents == 0) * 100) %>% pull()
+  ),
+  mean_count = c(
+    idaho_accidents_full_monthly %>% summarise(avg_accidents_per_month = mean(n_accidents)) %>% pull(),
+    idaho_accidents_full_quarterly %>% summarise(avg_accidents_per_qtr = mean(n_accidents)) %>% pull(),
+    idaho_accidents_full_yearly %>% summarise(avg_accidents_per_year = mean(n_accidents)) %>% pull()
+  ),
+  median_count = c(
+    idaho_accidents_full_monthly %>% summarise(median_accidents_per_month = median(n_accidents)) %>% pull(),
+    idaho_accidents_full_quarterly %>% summarise(median_accidents_per_qtr = median(n_accidents)) %>% pull(),
+    idaho_accidents_full_yearly %>% summarise(median_accidents_per_year = median(n_accidents)) %>% pull()
+  ),
+  sd = c(
+    idaho_accidents_full_monthly %>% summarise(sd_month = sd(n_accidents)) %>% pull(),
+    idaho_accidents_full_quarterly %>% summarise(sd_qtr = sd(n_accidents)) %>% pull(),
+    idaho_accidents_full_yearly %>% summarise(sd_year = sd(n_accidents)) %>% pull()
+  )
+)
+
+
+x_levels <- 0:35
+# Histogram, resolution = month
+idaho_accidents_full_monthly %>%
+  ggplot(aes(x = factor(n_accidents))) +
+  geom_bar(aes(y = after_stat(100 * count / sum(count)))) +
+  scale_y_continuous(breaks = seq(0, 100, 10), limits = c(0, 100)) +
+  scale_x_discrete(limits = as.character(x_levels)) +
+  labs(
+    x = "Fatal crash count",
+    y = "% of observations",
+    title = "Monthly resolution") +
+  theme_bw()
+
+# Histogram, resolution = quarterly
+idaho_accidents_full_quarterly %>%
+  ggplot(aes(x = factor(n_accidents))) +
+  geom_bar(aes(y = after_stat(100 * count / sum(count)))) +
+  scale_y_continuous(breaks = seq(0, 100, 10), limits = c(0, 100)) +
+  scale_x_discrete(limits = as.character(x_levels)) +
+  labs(x = "Fatal crash count", 
+       y = "% of observations",
+       title = "Quarterly resolution") +
+  theme_bw()
+
+# Histogram, resolution = yearly
+idaho_accidents_full_yearly %>%
+  ggplot(aes(x = factor(n_accidents))) +
+  geom_bar(aes(y = after_stat(100 * count / sum(count)))) +
+  scale_y_continuous(breaks = seq(0, 100, 10), limits = c(0, 100)) +
+  scale_x_discrete(limits = as.character(x_levels)) +
+  labs(x = "Fatal crash count", y = "% of observations", 
+       title = "Yearly resolution") +
+  theme_bw()
