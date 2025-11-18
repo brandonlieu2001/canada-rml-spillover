08dde7089f3c2f5c644e1d21e753eca74c306f9d
brandonlieu2001 <brandonlieu2001@gmail.com>
2025-11-18 09:57:23 -0800

    Fix DiDtrend plot for ID

--- code/03_did.R
+++ code/03_did.R
@@ -9,18 +9,20 @@ idaho_accidents_full_monthly <-
 
 # Build quarterly dataset
 idaho_accidents_full_quarterly <- idaho_accidents_full_monthly %>%
-  mutate(QUARTER = quarter(DATE, with_year = TRUE)) %>%
-  group_by(QUARTER, COUNTY) %>%
+  mutate(QUARTER = quarter(DATE, with_year = FALSE)) %>%
+  group_by(YEAR, QUARTER, COUNTY) %>%
   summarise(n_accidents = sum(n_accidents)) %>%
-  arrange(COUNTY, QUARTER) %>% 
+  arrange(COUNTY, YEAR, QUARTER) %>% 
   ungroup()
 
 # Mutate data to difference-in-difference format
 idaho_accidents_did_data <- idaho_accidents_full_quarterly %>%
   mutate(
-    POST = ifelse(QUARTER >= 2018.4, 1, 0),
+    POST = ifelse((YEAR >= 2019 | 
+                     YEAR == 2018 & QUARTER == 4), 1, 0),
     TREAT = ifelse(COUNTY %in% c(21), 1, 0), # 21 = Boundary County
-  )
+  ) %>% 
+  arrange(COUNTY, YEAR, QUARTER)
 
 # Descriptive statistics
 idaho_accidents_did_summary <- idaho_accidents_did_data %>%
@@ -33,45 +35,51 @@ idaho_accidents_did_summary <- idaho_accidents_did_data %>%
 
 # Plot DiD trends
 idaho_accidents_plot <- idaho_accidents_did_data %>%
-  group_by(TREAT, QUARTER) %>%
+  group_by(TREAT, YEAR, QUARTER) %>%
   summarise(
     mean_accidents = mean(n_accidents),
     n_observations = n()
-  )
+  ) 
+
+# Convert date to year-quarter format
+idaho_accidents_plot <- idaho_accidents_plot %>% 
+  mutate(date = sprintf("%i-%i-01", YEAR, (3 * (QUARTER - 1) + 1)))
 
 did_plot <-
-  ggplot(idaho_accidents_plot, aes(x = QUARTER, 
-                                   y = mean_accidents, 
-                                   color = as.factor(TREAT))) +
-  geom_line() +
+  ggplot(idaho_accidents_plot, 
+         aes(x = as.Date(date), y = mean_accidents, group = TREAT,
+               color = as.factor(TREAT))) +
   geom_point() +
-  geom_vline(xintercept = as.yearqtr("2018 Q4"),
-             linetype = "dashed", color = "black", linewidth = 1) +
-  annotate("text", x = as.yearqtr("2018 Q4"), y = 2.1,
-           label = "Canada RML (Oct 2018)", 
-           size = 3.5, hjust = -0.1, vjust = -0.5) +
-  scale_x_yearqtr(format = "%Y Q%q", n = 10) +
-  scale_y_continuous(limits = c(0, 2.2), breaks = seq(0, 2, 0.5)) +
-  scale_color_manual(
-    values = c("0" = "#0072B2", "1" = "#D55E00"),
-    labels = c("0" = "Control counties (remaining 43 counties)",
-               "1" = "Treatment county (Boundary County, ID)"),
-  ) +
+  geom_line() +
+  scale_x_date(date_labels = "%Y Q%1", date_breaks = "1 year") +
+  
+  geom_vline(xintercept = as.Date("2018-10-01"),
+            linetype = "dashed", color = "black", linewidth = 1) +
   labs(
     title = "Quarterly Fatal Accident Counts, Idaho",
     x = "Year-quarter",
     y = "Average number of accidents",
     color = "Treatment Group"
   ) +
-  theme_bw()
+  theme_bw() +
+  scale_color_manual(
+    values = c("0" = "#0072B2", "1" = "#D55E00"),
+    labels = c("0" = "Control counties (remaining 43 counties)",
+               "1" = "Treatment county (Boundary County, ID)")) +
+  annotate("text", x = as.Date("2018-10-01"), y = 2.0,
+           label = "Canada RML (Oct 2018)",
+           size = 3.5, hjust = -0.1, vjust = -0.5) 
+did_plot
+
 
 # Add mean labels
 did_plot + geom_label(
     data = idaho_accidents_did_summary,
     aes(
-      x = ifelse(POST == 0, as.yearqtr("2016 Q2"), as.yearqtr("2022 Q2")), 
+      x = as.Date(ifelse(POST == 0, "2016-04-01", "2022-04-01")), 
       y = mean_accidents + 0,
       label = paste0("Mean = ", round(mean_accidents, 3)),
       color = as.factor(TREAT)),
     show.legend = FALSE
   )
+
